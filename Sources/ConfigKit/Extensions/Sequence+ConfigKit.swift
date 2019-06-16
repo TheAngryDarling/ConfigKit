00d756602e41644bc38a59546a18963c3b0d29ee
Tyler Anger <tyler@tylers-macbook-pro.local>
2019-06-16 19:33:57 -0400

    Added missing methods that became available in later versions of Swift 4 (compactMap, removeAll)

---
+++ Sources/ConfigKit/Extensions/Sequence+ConfigKit.swift
@@ -0,0 +1,36 @@
+//
+//  Sequence+ConfigKit.swift
+//  ConfigKit
+//
+//  Created by Tyler Anger on 2019-06-16.
+//
+
+import Foundation
+
+internal extension Sequence {
+    #if !(os(macOS) || os(iOS) || os(tvOS) || os(watchOS)) && !swift(>=4.1)
+    func compactMap<ElementOfResult>(_ transform: (Element) throws -> ElementOfResult?) rethrows -> [ElementOfResult] {
+        var rtn: [ElementOfResult] = []
+        for v in self {
+            if let nV = try transform(v) { rtn.append(nV) }
+        }
+        return rtn
+    }
+    #endif
+    
+}
+
+internal extension Array {
+    #if !swift(>=4.1)
+    mutating func removeAll(where predicate: (Element) throws -> Bool) rethrows {
+        var idx = self.startIndex
+        while idx < self.endIndex {
+            if try predicate(self[idx]) {
+                self.remove(at: idx)
+            } else {
+                idx = self.index(after: idx)
+            }
+        }
+    }
+    #endif
+}
