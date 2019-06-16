25bf482ebb8b52a767c66e2221941b716d5cbf08
Tyler Anger <tyler@tylers-macbook-pro.local>
2019-06-16 19:31:51 -0400

    Updated comments to use new commenting standards Fixed issue using precompiler if statements failing in early versions of swift 4 on Linux

--- Sources/ConfigKit/Config+Coder.swift
+++ Sources/ConfigKit/Config+Coder.swift
@@ -9,15 +9,14 @@ import Foundation
 
 
 public extension Config {
-    /*
-    Coding indicator.  Used to indicate which type of coder to use when encoding and decoding configuration data
-     - json: Used to read from and write to json data.
-     - plist: Used to read from and write to plist data. (Currently only suppored on the mac/iOS/tvOS platforms)
-    */
+    
+    /// Coding indicator.  Used to indicate which type of coder to use when encoding and decoding configuration data
+    /// - json: Used to read from and write to json data.
+    /// - plist: Used to read from and write to plist data. (Currently only suppored on the mac/iOS/tvOS platforms)
     public enum ConfigCodingType {
         /// json coder.  Used to read from and write to json data.
         case json
-        #if os(macOS) || os(iOS) || os(tvOS)
+        #if os(macOS) || os(iOS) || os(tvOS) || os(watchOS) || swift(>=4.1)
         /// plist coder. Used to read from and write to plist data. (Currently only suppored on the mac/iOS/tvOS platforms)
         case plist
         #endif
@@ -26,41 +25,50 @@ public extension Config {
         public static func getType(fromExtension ext: String?) -> ConfigCodingType? {
             guard let val = ext else { return nil }
             if val == "json" { return .json }
-            #if os(macOS) || os(iOS) || os(tvOS)
+           #if os(macOS) || os(iOS) || os(tvOS) || os(watchOS) || swift(>=4.1)
             if val == "plist" { return .plist }
             #endif
             return nil
         }
         
-        // Returns the coding type for the specified mime type or nil of one could not be located
+        /// Returns the coding type for the specified mime type or nil of one could not be located
         public static func getType(fromMimeType mime: String?) -> ConfigCodingType? {
             guard let val = mime else { return nil }
             if val == "application/json" { return .json }
-            #if os(macOS) || os(iOS) || os(tvOS)
+            #if os(macOS) || os(iOS) || os(tvOS) || os(watchOS) || swift(>=4.1)
             if val == "application/x-plist" { return .plist }
             #endif
             return nil
         }
         
         
-        // Returns the Decoder for the given coder type
+        /// Returns the Decoder for the given coder type
         internal func getDecoder() -> ConfigDecoder {
+            #if os(macOS) || os(iOS) || os(tvOS) || os(watchOS) || swift(>=4.1)
             switch self {
-            case .json: return JSONDecoder()
-            #if os(macOS) || os(iOS) || os(tvOS)
-            case .plist: return PropertyListDecoder()
-            #endif
+                case .json: return JSONDecoder()
+                case .plist: return PropertyListDecoder()
             }
+            #else
+            switch self {
+                case .json: return JSONDecoder()
+            }
+            #endif
         }
         
-        // Returns the Encoder for the given coder type
+        /// Returns the Encoder for the given coder type
         internal func getEncoder() -> ConfigEncoder {
+            
+            #if os(macOS) || os(iOS) || os(tvOS) || os(watchOS) || swift(>=4.1)
             switch self {
-            case .json: return JSONEncoder()
-            #if os(macOS) || os(iOS) || os(tvOS)
-            case .plist: return PropertyListEncoder()
-            #endif
+                case .json: return JSONEncoder()
+                case .plist: return PropertyListEncoder()
             }
+            #else
+            switch self {
+                case .json: return JSONEncoder()
+            }
+            #endif
         }
         
     }
@@ -76,7 +84,7 @@ internal protocol ConfigEncoder {
 
 extension JSONDecoder: ConfigDecoder { }
 extension JSONEncoder: ConfigEncoder { }
-#if os(macOS) || os(iOS) || os(tvOS)
+#if os(macOS) || os(iOS) || os(tvOS) || os(watchOS) || swift(>=4.1)
 extension PropertyListDecoder: ConfigDecoder { }
 extension PropertyListEncoder: ConfigEncoder { }
 #endif
