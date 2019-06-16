ab904e82a0fe07f40c9351b71a06d470ce7974f7
Tyler Anger <tyler@tylers-macbook-pro.local>
2019-06-16 19:35:22 -0400

    Updated comments to use new commenting standards Fixed an issue where contacts where no being copied over when doing subsequent loads

--- Sources/ConfigKit/Config+Load.swift
+++ Sources/ConfigKit/Config+Load.swift
@@ -6,30 +6,23 @@
 //
 
 import Foundation
+import Dispatch
 
 public extension Config {
     
-    /**
-     Loads a configuration file from the given path. (Must be a .json or .plist file)
-     Any objects with the same name as objects comming in from the load function will be replaced
-     
-     - returns:
-     Returns the same instance of the config object so that you can repeatedly call load functions within the same line
-     eg.  config.loadFromEnv().load(fromFile: "global config.json").load(fromFile: "user config.json")
-     
-     - throws:
-     An error of type Config.Error.fileNotFound if the method could not locate the file
-     An error of type Config.Error.unknownConfigType if it could not identify the file type
-     A decoding error of the data is in an invalid format
-     
-     - parameters:
-        - fromFile: The config file to load.  Should have the extension of .json or .plist
-     
-    */
+    /// Loads a configuration file from the given path. (Must be a .json or .plist file)
+    /// Any objects with the same name as objects comming in from the load function will be replaced
+    ///
+    /// - parameter file: The config file to load.  Should have the extension of .json or .plist
+    ///
+    /// - returns: Returns the same instance of the config object so that you can repeatedly call load functions within the same line eg.  config.loadFromEnv().load(fromFile: "global config.json").load(fromFile: "user config.json")
+    /// - throws: An error of type Config.Error.fileNotFound if the method could not locate the file
+    /// An error of type Config.Error.unknownConfigType if it could not identify the file type
+    /// A decoding error of the data is in an invalid format
     @discardableResult
-    public func load(fromFile p: String) throws -> Config {
+    public func load(fromFile file: String) throws -> Config {
         // var path = p
-        let path = NSString(string: NSString(string: p).expandingTildeInPath).standardizingPath
+        let path = NSString(string: NSString(string: file).expandingTildeInPath).standardizingPath
         guard FileManager.default.fileExists(atPath: path) else { throw Error.fileNotFound(path) }
         
         guard let decoderType = ConfigCodingType.getType(fromExtension:URL(fileURLWithPath: path).pathExtension) else {
@@ -40,24 +33,17 @@ public extension Config {
         
     }
     
-    /**
-     Loads a configuration file from the given URL.
-     If the url is a file location, this method will call the load file method
-     Any objects with the same name as objects comming in from the load function will be replaced
-     
-     - returns:
-     Returns the same instance of the config object so that you can repeatedly call load functions within the same line
-     eg.  config.loadFromEnv().load(fromFile: "global config.json").load(fromFile: "user config.json")
-     
-     - throws:
-     An error of type Config.Error.fileNotFound if the method could not locate the file
-     An error of type Config.Error.unknownConfigType if it could not identify the file type
-     A decoding error of the data is in an invalid format
-     
-     - parameters:
-        - fromURL: The config url to load.  Should have the extension of .json or .plist or have a correct mimeType set
-     
-     */
+    
+     /// Loads a configuration file from the given URL.
+     /// If the url is a file location, this method will call the load file method
+     /// Any objects with the same name as objects comming in from the load function will be replaced
+     ///
+     /// - parameter url: The config url to load.  Should have the extension of .json or .plist or have a correct mimeType set
+     /// - returns: Returns the same instance of the config object so that you can repeatedly call load functions within the same line eg.  config.loadFromEnv().load(fromFile: "global config.json").load(fromFile: "user config.json")
+     ///
+     /// - throws: An error of type Config.Error.fileNotFound if the method could not locate the file
+     /// An error of type Config.Error.unknownConfigType if it could not identify the file type
+     /// A decoding error of the data is in an invalid format
     @discardableResult
     public func load(fromURL url: URL) throws -> Config {
         guard !url.isFileURL else { return try self.load(fromFile: url.path) }
@@ -91,42 +77,33 @@ public extension Config {
         return self
     }
     
-    /**
-     Loads a configuration from the given string an decoder type
-     This is a convienience function which calls the load(fromData, usingDecoderType) method
-     Any objects with the same name as objects comming in from the load function will be replaced
-     
-     - returns:
-     Returns the same instance of the config object so that you can repeatedly call load functions within the same line
-     eg.  config.loadFromEnv().load(fromFile: "global config.json").load(fromFile: "user config.json")
-     
-     - throws:
-     A decoding error of the data is in an invalid format
-     
-     - parameters:
-        - fromString: The string storing the configuration
-        - usingDecoderType: Indicating what decoder to use
-     */
+    /// Loads a configuration from the given string an decoder type
+    /// This is a convienience function which calls the load(fromData, usingDecoderType) method
+    /// Any objects with the same name as objects comming in from the load function will be replaced
+    ///
+    /// - parameters:
+    ///    - string: The string storing the configuration
+    ///    - decoderType: Indicating what decoder to use
+    ///
+    /// - returns: Returns the same instance of the config object so that you can repeatedly call load functions within the same line eg.  config.loadFromEnv().load(fromFile: "global config.json").load(fromFile: "user config.json")
+    ///
+    /// - throws: A decoding error of the data is in an invalid format
     @discardableResult
     public func load(fromString string: String, usingDecoderType decoderType: ConfigCodingType) throws -> Config {
         return try self.load(fromData: string.data(using: .utf8)!, usingDecoderType: decoderType)
     }
     
-    /**
-     Loads a configuration from the given data an decoder type
-     Any objects with the same name as objects comming in from the load function will be replaced
-     
-     - returns:
-     Returns the same instance of the config object so that you can repeatedly call load functions within the same line
-     eg.  config.loadFromEnv(.load(fromFile: "global config.json").load(fromFile: "user config.json")
-     
-     - throws:
-     A decoding error of the data is in an invalid format
-     
-     - parameters:
-        - fromData: The data storing the configuration
-        - usingDecoderType: Indicating what decoder to use
-     */
+    
+    /// Loads a configuration from the given data an decoder type
+    /// Any objects with the same name as objects comming in from the load function will be replaced
+    ///
+    /// - parameters:
+    ///    - data: The data storing the configuration
+    ///    - decoderType: Indicating what decoder to use
+    ///
+    /// - returns: Returns the same instance of the config object so that you can repeatedly call load functions within the same line eg.  config.loadFromEnv(.load(fromFile: "global config.json").load(fromFile: "user config.json")
+    ///
+    /// - throws: A decoding error of the data is in an invalid format
     @discardableResult
     public func load(fromData data: Data, usingDecoderType decoderType: ConfigCodingType) throws -> Config {
         let cfg: Config = try decoderType.getDecoder().decode(Config.self, from: data)
@@ -137,24 +114,22 @@ public extension Config {
     
     
    
-    /**
-     Loads environmental variables into parameters list (ProcessInfo.processInfo.environment)
-     This function will move any key value pairs into connection objects if they are in the following format:
-        {name}:connection_uri=value *Required
-        {name}:connection_auth_username=value
-        {name}:connection_auth_password=value
-        {name}:connection_auth_apikey=value
-        This also supports adding connection parameters using the following format:
-        {name}:connection_param:{param_name}=value
-     Any objects with the same name as objects comming in from the load function will be replaced
-     
-     - returns:
-     Returns the same instance of the config object so that you can repeatedly call load functions within the same line
-     eg.  config.loadFromEnv().load(fromFile: "global config.json").load(fromFile: "user config.json")
-     
-     - parameters:
-        - usingFilter: A way for you to filter which parameters to include or not
-     */
+    
+    /// Loads environmental variables into parameters list (ProcessInfo.processInfo.environment)
+    ///
+    /// This function will move any key value pairs into connection objects if they are in the following format:
+    ///  - {name}:connection_uri=value *Required
+    ///  - {name}:connection_auth_username=value
+    ///  - {name}:connection_auth_password=value
+    ///  - {name}:connection_auth_apikey=value
+    ///
+    ///    This also supports adding connection parameters using the following format:
+    ///    {name}:connection_param:{param_name}=value
+    /// Any objects with the same name as objects comming in from the load function will be replaced
+    ///
+    /// - parameter filter A way for you to filter which parameters to include or not
+    ///
+    /// - returns: Returns the same instance of the config object so that you can repeatedly call load functions within the same line eg.  config.loadFromEnv().load(fromFile: "global config.json").load(fromFile: "user config.json")
     @discardableResult
     public func loadFromEnv(usingFilter filter: (String)-> Bool = { _ in return true }) -> Config {
         var kv: [String: String] = [:]
@@ -165,26 +140,24 @@ public extension Config {
         return self.load(fromKeyValuePair: kv)
     }
     
-    /**
-     Loads command line parameters into parameters list (CommandLine.arguments)
-     This function will move any key value pairs into connection objects if they are in the following format:
-        {name}:connection_uri=value *Required
-        {name}:connection_auth_username=value
-        {name}:connection_auth_password=value
-        {name}:connection_auth_apikey=value
-        This also supports adding connection parameters using the following format:
-            {name}:connection_param:{param_name}=value
-     Any objects with the same name as objects comming in from the load function will be replaced
-     
-     - returns:
-     Returns the same instance of the config object so that you can repeatedly call load functions within the same line
-     eg.  config.loadFromEnv().load(fromFile: "global config.json").load(fromFile: "user config.json")
-     
-     - parameters:
-        - commandLineArgumentKeyPrefix: An indicator used to identify a parameter (--{paramName})
-        - keyValueSeperator: An indicator for where to split the key and value (--paramName=value)
-        - usingFilter: A way for you to filter which parameters to include or not
-     */
+    /// Loads command line parameters into parameters list (CommandLine.arguments)
+    ///
+    /// This function will move any key value pairs into connection objects if they are in the following format:
+    ///  - {name}:connection_uri=value *Required
+    ///  - {name}:connection_auth_username=value
+    ///  - {name}:connection_auth_password=value
+    ///  - {name}:connection_auth_apikey=value
+    ///
+    ///    This also supports adding connection parameters using the following format:
+    ///    {name}:connection_param:{param_name}=value
+    /// Any objects with the same name as objects comming in from the load function will be replaced
+    ///
+    /// - parameters:
+    ///    - commandLineArgumentKeyPrefix: An indicator used to identify a parameter (--{paramName})
+    ///    - seperator: An indicator for where to split the key and value (--paramName=value)
+    ///    - filter: A way for you to filter which parameters to include or not
+    ///
+    /// - returns: Returns the same instance of the config object so that you can repeatedly call load functions within the same line eg.  config.loadFromEnv().load(fromFile: "global config.json").load(fromFile: "user config.json")
     @discardableResult
     public func loadCommandLineArguments(commandLineArgumentKeyPrefix: String = "--",
                                          keyValueSeperator seperator: String = "=",
@@ -213,24 +186,22 @@ public extension Config {
         
     }
     
-    /**
-     Loads a dictionary into parameters list
-     This function will move any key value pairs into connection objects if they are in the following format:
-        {name}:connection_uri=value *Required
-        {name}:connection_auth_username=value
-        {name}:connection_auth_password=value
-        {name}:connection_auth_apikey=value
-        This also supports adding connection parameters using the following format:
-            {name}:connection_param:{param_name}=value
-     Any objects with the same name as objects comming in from the load function will be replaced
-     
-     - returns:
-     Returns the same instance of the config object so that you can repeatedly call load functions within the same line
-     eg.  config.loadFromEnv().load(fromFile: "global config.json").load(fromFile: "user config.json")
-     
-     - parameters:
-        - fromKeyValuePair: The values to load
-     */
+    
+    /// Loads a dictionary into parameters list
+    ///
+    /// This function will move any key value pairs into connection objects if they are in the following format:
+    ///  - {name}:connection_uri=value *Required
+    ///  - {name}:connection_auth_username=value
+    ///  - {name}:connection_auth_password=value
+    ///  - {name}:connection_auth_apikey=value
+    ///
+    ///    This also supports adding connection parameters using the following format:
+    ///    {name}:connection_param:{param_name}=value
+    /// Any objects with the same name as objects comming in from the load function will be replaced
+    ///
+    /// - parameter kv:The values to load
+    ///
+    /// - returns: Returns the same instance of the config object so that you can repeatedly call load functions within the same line eg.  config.loadFromEnv().load(fromFile: "global config.json").load(fromFile: "user config.json")
     @discardableResult
     public func load(fromKeyValuePair kv: [String: String]) -> Config {
         guard kv.count > 0 else { return self }
